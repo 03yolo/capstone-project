d086a88fd3400d70518cbce280c1578e9b662e7d
03yolo <lorenzemari.uni@gmail.com>
2025-07-02 20:24:47 +0800

    Delete capstone-project-poms directory

--- capstone-project-poms/capstone-project-poms/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace capstone_project_poms.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
